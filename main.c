51dfd7d530c028131a82cf8c1f4dfa4481198dce
Siddhant Tohan <siddhant.tohan.in@protonmail.com>
2026-07-12 11:20:31 +0530

    new: added basic command execution logic - added cd functionality - added external command functionality using fork and wait

--- main.c
+++ main.c
@@ -1,34 +1,84 @@
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 
 // TODOS;
-// 1. simple input loop. (DONE)
-// 2. call actual func via fork api.
-// 3. redirection support.
-// 4. pipe etc.
-// 5. catching signals.
+// 1. Core Shell
+// 2. Built Ins like CD etc.
+// 3. File Descriptors (<, >, >>)
+// 4. IPC (Pipes)
+// 5. Process Management.
+// 6. Extras (Quoted Strings, Environment Variables, History)
+
+// GLOBALS
+char cwd[PATH_MAX];
+
+void executeCommand(char *token) {
+    // For most commands, will prefer external implementation for now.
+    // For cd, will use chdir.
+    char *argv[64];
+    int argc = 0;
+    int pid = 0;
+    char *command;
+    if (!token)
+        return;
+    
+    command = token;
+    while (token != NULL) {
+        argv[argc++] = token;
+        token = strtok(NULL, " \t\r\n\v\f");
+    }
+    argv[argc++] = 0;
+
+    // Need special handling for cd.
+    if (strncmp(command, "cd", 2) == 0) {
+        if (argc != 3) {
+            printf("sshell: cd got too many arguments\n");
+            return;
+        }
+        if(chdir(argv[1]) == -1) {
+            printf("sshell: cd failed to change to directory %s\n", argv[1]);
+        }
+        return;
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        printf("Fork failed. Exiting");
+        return;
+    }
+
+    if (pid == 0) {
+        // Child
+        execvp(command, argv);
+    } else {
+        waitpid(pid, NULL, 0);
+        // Second param is for storing exit status. NULL means don't store.
+        // Last param is options. 0 is default blocking behaviour.
+    } 
+}
+
 void inputLoop() {
     char *buf = NULL;
     char *token = NULL;
     size_t buf_size = 0;
     ssize_t char_count = 0; 
     while (1) {
-        printf(">> ");
+        getcwd(cwd, sizeof(cwd));
+        printf("%s $ ", cwd);
         if ((char_count = getline(&buf, &buf_size, stdin)) == -1) {
             printf("Error encountered when reading. Aborting\n");
             free(buf);
             return;
         }
+        
         // Tokenize.
         token = strtok(buf, " \t\r\n\v\f");
-
-        while (token != NULL) {
-            printf("%s\n", token);
-            token = strtok(NULL, " \t\r\n\v\f");
-        }
+        executeCommand(token);
     }
 
 }
