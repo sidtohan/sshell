c289f40a8935371d20e73ea8c17d4f108da69776
Siddhant Tohan <siddhant.tohan.in@protonmail.com>
2026-07-13 22:38:34 +0530

    new: added basic redirection support. - need to fix program structure -> perferably separate tokenizer file

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #include "custom.h"
 
@@ -27,12 +28,20 @@ void executeCommand(char *token) {
     char *argv[64];
     int argc = 0;
     int pid = 0;
+    int redirect = 0;
     char *command;
+    
     if (!token)
         return;
     
     command = token;
     while (token != NULL) {
+        // Edge case: token is '>'. In this case, we have redirection on our hands.
+        if (*token == '>') {
+            redirect = 1;
+            token = strtok(NULL, " \t\r\n\v\f");
+            break;
+        }
         argv[argc++] = token;
         token = strtok(NULL, " \t\r\n\v\f");
     }
@@ -54,12 +63,38 @@ void executeCommand(char *token) {
     };
 
     pid = fork();
+    
     if (pid == -1) {
         printf("Fork failed. Exiting");
         return;
     }
 
     if (pid == 0) {
+        // Now need to handle redirect scenario.
+        // TODO: extend for custom commands also.. Might require args change.
+        if (redirect) {
+            int fd;
+            char *filename = token;
+            // Get file name
+            // Only consider the first param.. Rest can be ignored..
+            while (token != NULL) {
+                token = strtok(NULL, " \t\r\n\v\f");
+            }
+    
+            // For '>'. This is fine. TODO: add more like >>
+            fd = open(filename, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
+            if (fd == -1) {
+                printf("sshell: Error opening file for redirect\n");
+                exit(EXIT_FAILURE);
+            }
+    
+            // Overwrite STDOUT_FILENO with opened file..
+            dup2(fd, STDOUT_FILENO);
+            if(close(fd) == -1) {
+                printf("sshell: Error closing file internally for redirect\n");
+                exit(EXIT_FAILURE);
+            }
+        }
         // Child
         execvp(command, argv);
     } else {
