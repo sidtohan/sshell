55e02302e4c8c74b30c6d567e50c273991bef92f
Siddhant Tohan <siddhant.tohan.in@protonmail.com>
2026-07-25 16:22:19 +0530

    fix: make redirection slightly more resilient - can handle tokens leftover after > properly now

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,10 +1,13 @@
 #include "tokenizer.h"
+#include <string.h>
+
 void tokenize(char *buf) {
     int argc;
     char* argv[64];
     char* token = strtok(buf, " \t\r\n\v\f");
     char* command = NULL;
     int8_t redirect = 0;
+    int8_t temp1, temp2;
     char* redirect_fname = NULL;
 
     if (!token) // Empty
@@ -13,26 +16,26 @@ void tokenize(char *buf) {
     command = token;
     while (token != NULL) {
         // Special case: token is '>' or '>>'. In this case, we have redirection on our hands.
-        if (strncmp(token, ">", 2) == 0) {
-            redirect |= 1;
+        // Keep parsing
+        if ((temp1 = strncmp(token, ">>", 2) == 0) || (temp2 = strncmp(token, ">", 2) == 0)) {
+            if (redirect) {
+                printf("sshell: Multiple redirects detected.\n"); 
+                // Exhaust all tokens.
+                while (token != NULL) 
+                    token = strtok(NULL, " \t\r\n\v\f");
+                return;
+            }
+            // 0th bit: for trunc
+            // 1st bit: for append
+            redirect |= (temp1 ? APP_BIT : TRUNC_BIT);
             token = strtok(NULL, " \t\r\n\v\f");
             redirect_fname = token;
-            break;
-        } else if (strncmp(token, ">>", 2) == 0) {
-            redirect |= 2;
+            // Do it twice. Because we don't want the filename as one of the args.
             token = strtok(NULL, " \t\r\n\v\f");
-            redirect_fname = token;
-            break;
+            continue;
         }
         argv[argc++] = token;
         token = strtok(NULL, " \t\r\n\v\f");
-    }
-
-    if (redirect) {
-        // Currently, any token after the first token that follows '>'
-        // or '>>' is discarded..
-        while (token != NULL)
-            token = strtok(NULL, " \t\r\n\v\f");
     }
 
     argv[argc] = 0;
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -2,4 +2,6 @@
 #include <string.h>
 #include "command.h"
 
+#define TRUNC_BIT 1
+#define APP_BIT 2
 extern void tokenize(char *buf);
